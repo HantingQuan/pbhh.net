1aedb551fbed9270116235fb98afd17d9187478b
montmorill <montmorill@outlook.com>
2026-07-13 00:55:45 +0800

    Update index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,11 @@
-import { readFileSync } from 'node:fs'
-import http from 'node:http'
 import yaml from '@rollup/plugin-yaml'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
-const httpRedirect = {
-  name: 'http-redirect',
-  configurePreviewServer() {
-    http.createServer((req, res) => {
-      res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` })
-      res.end()
-    }).listen(80)
-  },
-}
-
-function tryReadFileSync(path: string) {
-  try {
-    return readFileSync(path)
-  }
-  catch {
-    return ''
-  }
-}
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), yaml(), httpRedirect],
+  plugins: [vue(), tailwindcss(), yaml()],
   resolve: { alias: {
     '@': '/src',
     'server': '/server',
@@ -37,14 +16,6 @@ export default defineConfig({
         target: 'http://localhost:3000',
         ws: true,
       },
-    },
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: 443,
-    https: {
-      key: tryReadFileSync('/ssl/cert.key'),
-      cert: tryReadFileSync('/ssl/cert.pem'),
     },
   },
 })
